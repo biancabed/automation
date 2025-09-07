d288574f6ac33f255416242fae2124d5997b9cb4
biancabed <bedrulebianca715@gmail.com>
2025-09-07 17:09:38 +0300

    Update aplicatie.py

--- aplicatie.py
+++ aplicatie.py
@@ -28,7 +28,7 @@ st.title("💨 Comparative Energy Output Analysis – 30 MW Wind Farm")
 
 st.markdown(" 🎯 Purpose of this application:
 This tool compares the annual energy output of three wind farm configurations, each totaling 30 MW, using only one turbine type:
-
+")
 - 12 × Nordex N100 (2.5 MW)  
 - 10 × Vestas V90 (3 MW)  
 - 6 × Repower M5 (5 MW)  
